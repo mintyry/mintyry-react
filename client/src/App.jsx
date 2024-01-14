3f3272ce25c5b12462010f76b831def6be0246c3
mintyry <gitmintyry@gmail.com>
2024-01-13 20:36:18 -0800

    got click events to work for welcome page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './styles/App.css'
 import Welcome from './components/Welcome';
-import Section from './components/Section';
 
 function App() {
 
-  const [showPortfolio, setShowPortfolio] = useState(false);
-
-  useEffect(() => {
-    // runs code when state changes
-    // function for event
-    const clickWelcome = () => {
-      setShowPortfolio(true);
-    };
-
-    // event listener when user clicks on welcome page
-    document.addEventListener('click', clickWelcome);
-
-    return () => {
-      document.removeEventListener('click', clickWelcome);
-    };
-
-  }, []);//ends useEffect
-
-// display welcome page if showPortfolio is false, when clicked (true), shows Section
   return (
     <>
-    {
-      !showPortfolio ? ( <Welcome /> ) : (<Section />)
-    }
+      <Welcome />
     </>
   )
 }
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,63 @@
 import '../styles/Welcome.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Section from './Section'
 
 function Welcome() {
+    // initially setting states to false (dont show)
+    const [showMessage, setMessage] = useState(false);
+    const [showSection, setShowSection] = useState(false);
+
+    // useEffect: renders content conditionally
+    // This one will progress welcome message when app is clicked anywhere.
+    useEffect(() => {
+        // when this function runs, setMessage state is changed to true, and will display corresponding message.
+        function clickWelcome() {
+            setMessage(true);
+        };
+        // event listener on page; when page is clicked, runs clickWelcome()
+        document.addEventListener('click', clickWelcome);
+        // cleanup function; removes event listener to run clickWelcome()
+        return () => {
+            document.removeEventListener('click', clickWelcome);
+        };
+    }, []);//ends useEffect
+
+    // This useEffect is for leading to the next component (Section);
+    useEffect(() => {
+        // function changes setShowFunction state to true
+        function openSection() {
+            setShowSection(true);
+        };
+        // if showMessage is truthy, it will add an event listener
+        if (showMessage) {
+            document.addEventListener('click', openSection);
+            // cleanup fn to remove event listener when component is dismounted
+            return () => {
+                document.removeEventListener('click', openSection);
+            }
+        }
+        // this dependency ensures that this useeffect runs when showMessage's value changes
+    }, [showMessage]);
+
+    // if it showSection is falsy, then show the welcome message...
+        //if showMessage is falsy, show Welcome, otherwise, show horcrux line
+    //if showSection is truthy, we show Section component.
     return (
-        <section>
-            <h1>Welcome</h1>
-        </section>
+        <>
+            {
+                !showSection
+                    ?
+                    (
+                        !showMessage ?
+                            <h1>Welcome...</h1> :
+                            <h1>to my <strong>horcrux...</strong></h1>
+                    )
+                    :
+                    (
+                        <Section />
+                    )
+            }
+        </>
     );
 }
 
