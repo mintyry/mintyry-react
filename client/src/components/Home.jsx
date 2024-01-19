b79a3ca7c4edc99f9ebcebcd06b1069a34dc87b7
mintyry <gitmintyry@gmail.com>
2024-01-19 01:08:40 -0800

    fixed clipping on mobile by making width 100vw sted of %

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,19 +2,25 @@
 import { useState, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+// import '../styles/Home.scss';
 
 
 function Home() {
     // changes body bg color
-    document.body.style = 'background: teal;';
+    document.body.style = 'background: teal; ';
 
 
     return (
         <>
             <Header />
-            <section>
-                <h1>This worked!</h1>
-            </section>
+            <section className="ripple-background">
+        <h1>This worked!</h1>
+        <div className="circle xxlarge shade1"></div>
+        <div className="circle xlarge shade2"></div>
+        <div className="circle large shade3"></div>
+        <div className="circle medium shade4"></div>
+        <div className="circle small shade5"></div>
+      </section>
             <Footer />
         </>
     );
