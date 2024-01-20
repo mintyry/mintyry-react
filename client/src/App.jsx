cd009a0e8377484b3e7abcb33b48996d840b6ad2
mintyry <gitmintyry@gmail.com>
2024-01-20 15:48:12 -0800

    commented App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import Welcome from './pages/Welcome';
 import { Outlet, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Header from './components/Header';
@@ -8,11 +7,13 @@ import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 
 function App() {
+  // using location to read what path/page we are on; states are for hiding header and footer
   const location = useLocation();
   const [hideHeader, setHideHeader] = useState(false);
   const [hideFooterNav, setHideFooterNav] = useState(false);
 
   useEffect(() => {
+    // if we are on root path (welcome message), hide header and footer
     if (location.pathname === "/") {
       setHideHeader(true);
       setHideFooterNav(true);
@@ -20,17 +21,19 @@ function App() {
       setHideHeader(false);
       setHideFooterNav(false);
     }
+    // runs when url path changes
   }, [location])
 
   return (
     <>
-      {/* short circuit to hide header if at root path (Welcome) */}
+      {/* short circuit to hide header if at root path (Welcome), literally reads: no hiding header and display header component */}
       {!hideHeader && <Header />}
 
+      {/* outlet injects page here */}
       <Outlet />
 
       {/* short circuit to hide header if at root path (Welcome) */}
-      {!hideFooterNav && 
+      {!hideFooterNav &&
         <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3} marginBottom={5}>
           <BottomNavigation showLabels className='transparent'>
             <Footer />
