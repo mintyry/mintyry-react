176472abcbe59d23ba5b65e2db3ac37472ac553d
mintyry <gitmintyry@gmail.com>
2024-01-19 16:24:14 -0800

    added footer with button group

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,43 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-function Footer() {
-    return (
-      <p> Hi </p>
-    );
+// function Footer() {
+//   return (
+//     <footer>
+//       <ButtonGroup variant="text" aria-label="text button group">
+//         <Button>One</Button>
+//         <Button>Two</Button>
+//         <Button>Three</Button>
+//       </ButtonGroup>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+import '../styles/Footer.css';
+
+export default function Footer() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > *': {
+          m: 1,
+        },
+      }}
+    >
+
+      <ButtonGroup variant="text" aria-label="text button group" id="linkColor">
+        <Button>Reach</Button>
+        <Button>Read</Button>
+        <Button>Relish</Button>
+      </ButtonGroup>
+    </Box>
+  );
 }
-
-export default Footer;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,9 @@
 import { useState, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-// import '../styles/Home.scss';
+import '../styles/Home.scss';
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
 
 
 function Home() {
@@ -13,15 +15,16 @@ function Home() {
     return (
         <>
             <Header />
-            <section className="ripple-background">
-        <h1>This worked!</h1>
-        <div className="circle xxlarge shade1"></div>
-        <div className="circle xlarge shade2"></div>
-        <div className="circle large shade3"></div>
-        <div className="circle medium shade4"></div>
-        <div className="circle small shade5"></div>
-      </section>
-            <Footer />
+            <section>
+                <p>hiiiii</p>
+            </section>
+
+            <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3} className="transparent">
+                <BottomNavigation showLabels className='transparent'>
+                    <Footer />
+                </BottomNavigation>
+            </Box>
+
         </>
     );
 }
