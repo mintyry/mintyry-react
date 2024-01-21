c1a2b2177414bebd182197655e504e9d4a6532fb
mintyry <gitmintyry@gmail.com>
2024-01-21 15:15:46 -0800

    linked head glitch text to home, removed scanlines, edited about me

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import '../styles/Header.scss';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header style={{paddingTop:"1em"}}>
+    <header style={{ paddingTop: "1em" }}>
 
-      <div className="glitch" data-text="MINTY.RY">MINTY.RY</div>
-      <div className="glow">MINTY.RY</div>
-      <p style={{paddingBottom:"1em"}} className="subtitle">BY RYAN ECLARIN</p>
+      {/* <div className="scanlines"></div> */}
+      <Link to="/home" className="noTextDec">
+        <div className="glitch" data-text="MINTY.RY">MINTY.RY</div>
+        <div className="glow">MINTY.RY</div>
+      </Link>
+      <p style={{ paddingBottom: "1em" }} className="subtitle">BY RYAN ECLARIN</p>
 
-      <div className="scanlines"></div>
     </header>
   );
 }
