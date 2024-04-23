1116a26c5926de14263c5fc34c247d84c0bd8a3c
mintyry <gitmintyry@gmail.com>
2024-04-22 18:33:08 -0700

    resume, python, java, etc

--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -23,31 +23,36 @@ function Ticker() {
                         <Icon sx={{ width: "3em" }} className="fa-brands fa-html5 iconcolor" />
                         <Icon sx={{ width: "3em" }} className="fa-brands fa-css3-alt" />
                         <Icon sx={{ width: "3em" }} className="fa-brands fa-js iconcolor" />
+                        <Icon sx={{ width: "3em" }} className="fa-brands fa-python" />
+                        <Icon sx={{ width: "3em" }} className="fa-brands fa-brands fa-java iconcolor" />
                         <Icon sx={{ width: "3em" }} className="fa-brands fa-square-github" />
                         <Icon sx={{ width: "3em" }} className="fa-brands fa-git-alt iconcolor" />
                         <Icon sx={{ width: "3em" }} className="fa-brands fa-bootstrap" />
                         <Icon sx={{ width: "3em" }} className="fa-brands fa-node-js iconcolor" />
-                        <Icon sx={{ width: "3em" }} className="fa-solid fa-database" />
-                        <Icon sx={{ width: "3em" }} className="fa-brands fa-react iconcolor" />
-                        <Icon sx={{ width: "3em" }} className="fa-brands fa-stripe" />
-                        <Icon sx={{ width: "3em" }} className="fa-brands fa-figma iconcolor" />&nbsp;&nbsp;&nbsp;
+                        <Icon sx={{ width: "3em" }} className="fa-solid fa-flask" />
+                        <Icon sx={{ width: "3em" }} className="fa-solid fa-database iconcolor" />
+                        <Icon sx={{ width: "3em" }} className="fa-brands fa-react" />
+                        <Icon sx={{ width: "3em" }} className="fa-brands fa-stripe iconcolor" />
+                        <Icon sx={{ width: "3em" }} className="fa-brands fa-figma" />&nbsp;&nbsp;&nbsp;
                         {/* from devicon */}
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-mongodb-plain"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-sequelize-plain iconcolor"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-webpack-plain"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-apple-original iconcolor"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-jquery-plain"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-handlebars-plain iconcolor"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-bulma-plain"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-materialui-plain iconcolor"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-mysql-plain"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-vscode-plain iconcolor"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-mongodb-plain iconcolor"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-sequelize-plain"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-webpack-plain iconcolor"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-apple-original"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-jquery-plain iconcolor"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-handlebars-plain "></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-bulma-plain iconcolor"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-materialui-plain"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-mysql-plain iconcolor"></i>
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-vscode-plain "></i>
+                        <i style={{ fontSize: "1.5rem", marginRight: "1.5em" }} className="devicon-intellij-plain iconcolor"></i>
                         <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-chrome-plain"></i>
                         <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-eslint-original iconcolor"></i>
                         <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-express-original"></i>
                         <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-heroku-original iconcolor"></i>
                         <i style={{ fontSize: "1.5rem", marginRight: "1.5em" }} className="devicon-graphql-plain"></i>
-                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-devicon-plain iconcolor"></i>
+          
+                        <i style={{ fontSize: "1.7rem", marginRight: "1.5em" }} className="devicon-devicon-plain"></i>
                     </marquee>
                 </Grid>
             </Grid>
